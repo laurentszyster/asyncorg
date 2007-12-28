5ece9f24b324617f78b797947196b5ddfa2676c6
laurentszyster <laurentszyster@ce82f516-9541-0410-b588-ed97c34d1957>
2007-12-28 06:03:50 +0000

    Sparse documentation, a starting point.

---
+++ trunk/doc/index.js
@@ -0,0 +1,764 @@
+javadoc.index = {
+  "ALPHANUMERIC": [
+    "org.async.simple.Strings.ALPHANUMERIC"
+    ], 
+  "Bytes": [
+    "org.async.simple.Bytes"
+    ], 
+  "BytesProducer": [
+    "org.async.chat.BytesProducer"
+    ], 
+  "Call": [
+    "org.async.core.Call"
+    ], 
+  "Call.Closure": [
+    "org.async.core.Call.Closure"
+    ], 
+  "Call.Fork": [
+    "org.async.core.Call.Fork"
+    ], 
+  "Call.Join": [
+    "org.async.core.Call.Join"
+    ], 
+  "Call.Sleep": [
+    "org.async.core.Call.Sleep"
+    ], 
+  "Call.Step": [
+    "org.async.core.Call.Step"
+    ], 
+  "Collector": [
+    "org.async.chat.Collector", 
+    "org.async.net.Collector"
+    ], 
+  "Dispatcher": [
+    "org.async.chat.Dispatcher", 
+    "org.async.core.Dispatcher", 
+    "org.async.net.Dispatcher"
+    ], 
+  "FileClassLoader": [
+    "org.async.simple.FileClassLoader"
+    ], 
+  "Function": [
+    "org.async.core.Function"
+    ], 
+  "GuardCollector": [
+    "org.async.chat.GuardCollector"
+    ], 
+  "Listen": [
+    "org.async.core.Listen"
+    ], 
+  "Loginfo": [
+    "org.async.core.Loginfo"
+    ], 
+  "Loop": [
+    "org.async.core.Loop"
+    ], 
+  "Objects": [
+    "org.async.simple.Objects"
+    ], 
+  "Producer": [
+    "org.async.chat.Producer"
+    ], 
+  "Scheduled": [
+    "org.async.core.Scheduled"
+    ], 
+  "Static": [
+    "org.async.core.Static"
+    ], 
+  "Stream": [
+    "org.async.core.Stream"
+    ], 
+  "Strings": [
+    "org.async.simple.Strings"
+    ], 
+  "StringsCollector": [
+    "org.async.chat.StringsCollector"
+    ], 
+  "StringsProducer": [
+    "org.async.chat.StringsProducer"
+    ], 
+  "UTF8": [
+    "org.async.simple.Bytes.UTF8"
+    ], 
+  "accept": [
+    "org.async.core.Listen.accept"
+    ], 
+  "add": [
+    "org.async.core.Call.Fork.add"
+    ], 
+  "apply": [
+    "org.async.core.Scheduled.apply", 
+    "org.async.core.Function.apply", 
+    "org.async.core.Call.Closure.apply", 
+    "org.async.core.Call.Fork.apply", 
+    "org.async.core.Call.Step.apply", 
+    "org.async.core.Call.Join.apply", 
+    "org.async.core.Call.Sleep.apply"
+    ], 
+  "bind": [
+    "org.async.core.Dispatcher.bind"
+    ], 
+  "bytesIn": [
+    "org.async.core.Dispatcher.bytesIn"
+    ], 
+  "bytesOut": [
+    "org.async.core.Dispatcher.bytesOut"
+    ], 
+  "close": [
+    "org.async.core.Dispatcher.close"
+    ], 
+  "closeWhenDone": [
+    "org.async.core.Stream.closeWhenDone"
+    ], 
+  "collect": [
+    "org.async.chat.Dispatcher.collect", 
+    "org.async.core.Stream.collect", 
+    "org.async.core.Loop.collect", 
+    "org.async.net.Dispatcher.collect", 
+    "org.async.net.Collector.collect"
+    ], 
+  "compareTo": [
+    "org.async.core.Scheduled.compareTo"
+    ], 
+  "concurrency": [
+    "org.async.core.Loop.concurrency"
+    ], 
+  "connect": [
+    "org.async.core.Dispatcher.connect"
+    ], 
+  "connected": [
+    "org.async.core.Dispatcher.connected"
+    ], 
+  "continuation": [
+    "org.async.core.Call.continuation"
+    ], 
+  "decode": [
+    "org.async.simple.Bytes.decode"
+    ], 
+  "dict": [
+    "org.async.simple.Objects.dict"
+    ], 
+  "dispatch": [
+    "org.async.core.Loop.dispatch"
+    ], 
+  "encode": [
+    "org.async.simple.Bytes.encode"
+    ], 
+  "err": [
+    "org.async.core.Loginfo.err"
+    ], 
+  "exits": [
+    "org.async.core.Loop.exits"
+    ], 
+  "extend": [
+    "org.async.simple.Objects.extend"
+    ], 
+  "finalize": [
+    "org.async.core.Call.finalize"
+    ], 
+  "find": [
+    "org.async.simple.Bytes.find"
+    ], 
+  "findClass": [
+    "org.async.simple.FileClassLoader.findClass"
+    ], 
+  "fork": [
+    "org.async.core.Call.fork"
+    ], 
+  "handleAccept": [
+    "org.async.core.Stream.handleAccept", 
+    "org.async.core.Dispatcher.handleAccept"
+    ], 
+  "handleClose": [
+    "org.async.core.Dispatcher.handleClose"
+    ], 
+  "handleCollect": [
+    "org.async.net.Dispatcher.handleCollect"
+    ], 
+  "handleCollected": [
+    "org.async.net.Dispatcher.handleCollected"
+    ], 
+  "handleConnect": [
+    "org.async.core.Listen.handleConnect", 
+    "org.async.core.Dispatcher.handleConnect"
+    ], 
+  "handleData": [
+    "org.async.chat.StringsCollector.handleData", 
+    "org.async.chat.GuardCollector.handleData", 
+    "org.async.chat.Dispatcher.handleData", 
+    "org.async.chat.Collector.handleData"
+    ], 
+  "handleError": [
+    "org.async.core.Dispatcher.handleError"
+    ], 
+  "handleRead": [
+    "org.async.core.Stream.handleRead", 
+    "org.async.core.Listen.handleRead", 
+    "org.async.core.Dispatcher.handleRead"
+    ], 
+  "handleTerminator": [
+    "org.async.chat.StringsCollector.handleTerminator", 
+    "org.async.chat.GuardCollector.handleTerminator", 
+    "org.async.chat.Dispatcher.handleTerminator", 
+    "org.async.chat.Collector.handleTerminator"
+    ], 
+  "handleWrite": [
+    "org.async.core.Stream.handleWrite", 
+    "org.async.core.Listen.handleWrite", 
+    "org.async.core.Dispatcher.handleWrite"
+    ], 
+  "hookShutdown": [
+    "org.async.core.Loop.hookShutdown"
+    ], 
+  "iter": [
+    "org.async.simple.Objects.iter"
+    ], 
+  "join": [
+    "org.async.core.Call.join", 
+    "org.async.simple.Strings.join"
+    ], 
+  "list": [
+    "org.async.simple.Objects.list"
+    ], 
+  "listen": [
+    "org.async.core.Listen.listen"
+    ], 
+  "log": [
+    "org.async.core.Loop.log", 
+    "org.async.core.Dispatcher.log"
+    ], 
+  "loop": [
+    "org.async.core.Static.loop"
+    ], 
+  "more": [
+    "org.async.chat.StringsProducer.more", 
+    "org.async.chat.Producer.more", 
+    "org.async.chat.BytesProducer.more"
+    ], 
+  "now": [
+    "org.async.core.Loop.now"
+    ], 
+  "out": [
+    "org.async.core.Loginfo.out"
+    ], 
+  "precision": [
+    "org.async.core.Loop.precision"
+    ], 
+  "produce": [
+    "org.async.chat.Dispatcher.produce", 
+    "org.async.core.Stream.produce", 
+    "org.async.net.Dispatcher.produce"
+    ], 
+  "pull": [
+    "org.async.core.Stream.pull"
+    ], 
+  "push": [
+    "org.async.chat.Dispatcher.push", 
+    "org.async.core.Stream.push", 
+    "org.async.net.Dispatcher.push"
+    ], 
+  "random": [
+    "org.async.simple.Strings.random"
+    ], 
+  "readable": [
+    "org.async.core.Stream.readable", 
+    "org.async.core.Listen.readable", 
+    "org.async.core.Dispatcher.readable"
+    ], 
+  "recv": [
+    "org.async.core.Dispatcher.recv"
+    ], 
+  "recvfrom": [
+    "org.async.core.Dispatcher.recvfrom"
+    ], 
+  "schedule": [
+    "org.async.core.Loop.schedule"
+    ], 
+  "scheduled": [
+    "org.async.core.Loop.scheduled"
+    ], 
+  "send": [
+    "org.async.core.Dispatcher.send"
+    ], 
+  "sendto": [
+    "org.async.core.Dispatcher.sendto"
+    ], 
+  "set": [
+    "org.async.simple.Objects.set"
+    ], 
+  "setLogger": [
+    "org.async.core.Loop.setLogger"
+    ], 
+  "setPrecision": [
+    "org.async.core.Loop.setPrecision"
+    ], 
+  "setTerminator": [
+    "org.async.chat.Dispatcher.setTerminator"
+    ], 
+  "split": [
+    "org.async.simple.Strings.split"
+    ], 
+  "stalled": [
+    "org.async.chat.StringsProducer.stalled", 
+    "org.async.chat.Producer.stalled", 
+    "org.async.chat.BytesProducer.stalled"
+    ], 
+  "step": [
+    "org.async.core.Call.step"
+    ], 
+  "tail": [
+    "org.async.core.Call.tail"
+    ], 
+  "terminate": [
+    "org.async.net.Collector.terminate"
+    ], 
+  "timeout": [
+    "org.async.core.Loop.timeout"
+    ], 
+  "traceback": [
+    "org.async.core.Loginfo.traceback"
+    ], 
+  "update": [
+    "org.async.simple.Objects.update"
+    ], 
+  "when": [
+    "org.async.core.Scheduled.when", 
+    "org.async.core.Dispatcher.when"
+    ], 
+  "whenIn": [
+    "org.async.core.Dispatcher.whenIn"
+    ], 
+  "whenOut": [
+    "org.async.core.Dispatcher.whenOut"
+    ], 
+  "wrap": [
+    "org.async.chat.StringsProducer.wrap", 
+    "org.async.chat.BytesProducer.wrap"
+    ], 
+  "writable": [
+    "org.async.chat.Dispatcher.writable", 
+    "org.async.core.Listen.writable", 
+    "org.async.core.Dispatcher.writable", 
+    "org.async.net.Dispatcher.writable"
+    ]
+  };
+javadoc.packages = {
+  "org.async.chat": [
+    "StringsProducer", 
+    "StringsCollector", 
+    "Producer", 
+    "GuardCollector", 
+    "Dispatcher", 
+    "Collector", 
+    "BytesProducer"
+    ], 
+  "org.async.core": [
+    "Stream", 
+    "Static", 
+    "Scheduled", 
+    "Loop", 
+    "Loginfo", 
+    "Listen", 
+    "Function", 
+    "Dispatcher", 
+    "Call", 
+    "Call.Closure", 
+    "Call.Fork", 
+    "Call.Step", 
+    "Call.Join", 
+    "Call.Sleep"
+    ], 
+  "org.async.net": [
+    "Dispatcher", 
+    "Collector"
+    ], 
+  "org.async.simple": [
+    "Strings", 
+    "Objects", 
+    "FileClassLoader", 
+    "Bytes"
+    ]
+  };
+javadoc.classes = {
+  "org.async.chat.BytesProducer": {
+    "constants": [], 
+    "extends": "java.lang.Object", 
+    "functions": [
+      "wrap"
+      ], 
+    "implements": [
+      "org.async.chat.Producer"
+      ], 
+    "members": [], 
+    "methods": [
+      "stalled", 
+      "more"
+      ]
+    }, 
+  "org.async.chat.Collector": {
+    "constants": [], 
+    "functions": [], 
+    "implements": [], 
+    "members": [], 
+    "methods": [
+      "handleData", 
+      "handleTerminator"
+      ]
+    }, 
+  "org.async.chat.Dispatcher": {
+    "constants": [], 
+    "extends": "org.async.core.Stream", 
+    "functions": [], 
+    "implements": [], 
+    "members": [], 
+    "methods": [
+      "setTerminator", 
+      "push", 
+      "collect", 
+      "writable", 
+      "produce", 
+      "handleData", 
+      "handleTerminator"
+      ]
+    }, 
+  "org.async.chat.GuardCollector": {
+    "constants": [], 
+    "extends": "java.lang.Object", 
+    "functions": [], 
+    "implements": [
+      "org.async.chat.Collector"
+      ], 
+    "members": [], 
+    "methods": [
+      "handleData", 
+      "handleTerminator"
+      ]
+    }, 
+  "org.async.chat.Producer": {
+    "constants": [], 
+    "functions": [], 
+    "implements": [], 
+    "members": [], 
+    "methods": [
+      "stalled", 
+      "more"
+      ]
+    }, 
+  "org.async.chat.StringsCollector": {
+    "constants": [], 
+    "extends": "java.lang.Object", 
+    "functions": [], 
+    "implements": [
+      "org.async.chat.Collector"
+      ], 
+    "members": [], 
+    "methods": [
+      "handleData", 
+      "handleTerminator"
+      ]
+    }, 
+  "org.async.chat.StringsProducer": {
+    "constants": [], 
+    "extends": "java.lang.Object", 
+    "functions": [
+      "wrap"
+      ], 
+    "implements": [
+      "org.async.chat.Producer"
+      ], 
+    "members": [], 
+    "methods": [
+      "stalled", 
+      "more"
+      ]
+    }, 
+  "org.async.core.Call": {
+    "constants": [], 
+    "contains": [
+      "org.async.core.Call.Closure", 
+      "org.async.core.Call.Fork", 
+      "org.async.core.Call.Step", 
+      "org.async.core.Call.Join", 
+      "org.async.core.Call.Sleep"
+      ], 
+    "extends": "java.lang.Object", 
+    "functions": [
+      "tail", 
+      "fork", 
+      "step", 
+      "join"
+      ], 
+    "implements": [
+      "org.async.core.Function"
+      ], 
+    "members": [
+      "continuation"
+      ], 
+    "methods": [
+      "finalize"
+      ]
+    }, 
+  "org.async.core.Call.Closure": {
+    "constants": [], 
+    "extends": "org.async.core.Call", 
+    "functions": [], 
+    "implements": [], 
+    "members": [], 
+    "methods": [
+      "apply"
+      ]
+    }, 
+  "org.async.core.Call.Fork": {
+    "constants": [], 
+    "extends": "org.async.core.Call", 
+    "functions": [], 
+    "implements": [], 
+    "members": [], 
+    "methods": [
+      "add", 
+      "apply"
+      ]
+    }, 
+  "org.async.core.Call.Join": {
+    "constants": [], 
+    "extends": "org.async.core.Call", 
+    "functions": [], 
+    "implements": [], 
+    "members": [], 
+    "methods": [
+      "apply"
+      ]
+    }, 
+  "org.async.core.Call.Sleep": {
+    "constants": [], 
+    "extends": "org.async.core.Call", 
+    "functions": [], 
+    "implements": [], 
+    "members": [], 
+    "methods": [
+      "apply"
+      ]
+    }, 
+  "org.async.core.Call.Step": {
+    "constants": [], 
+    "extends": "org.async.core.Call", 
+    "functions": [], 
+    "implements": [], 
+    "members": [], 
+    "methods": [
+      "apply"
+      ]
+    }, 
+  "org.async.core.Dispatcher": {
+    "constants": [], 
+    "extends": "org.async.core.Call", 
+    "functions": [], 
+    "implements": [], 
+    "members": [
+      "when", 
+      "whenIn", 
+      "whenOut", 
+      "bytesIn", 
+      "bytesOut"
+      ], 
+    "methods": [
+      "log", 
+      "connected", 
+      "connect", 
+      "recv", 
+      "send", 
+      "bind", 
+      "recvfrom", 
+      "sendto", 
+      "close", 
+      "writable", 
+      "readable", 
+      "handleAccept", 
+      "handleConnect", 
+      "handleWrite", 
+      "handleRead", 
+      "handleClose", 
+      "handleError"
+      ]
+    }, 
+  "org.async.core.Function": {
+    "constants": [], 
+    "functions": [], 
+    "implements": [], 
+    "members": [], 
+    "methods": [
+      "apply"
+      ]
+    }, 
+  "org.async.core.Listen": {
+    "constants": [], 
+    "extends": "org.async.core.Dispatcher", 
+    "functions": [], 
+    "implements": [], 
+    "members": [], 
+    "methods": [
+      "listen", 
+      "accept", 
+      "writable", 
+      "readable", 
+      "handleConnect", 
+      "handleRead", 
+      "handleWrite"
+      ]
+    }, 
+  "org.async.core.Loginfo": {
+    "constants": [], 
+    "functions": [], 
+    "implements": [], 
+    "members": [], 
+    "methods": [
+      "out", 
+      "err", 
+      "traceback"
+      ]
+    }, 
+  "org.async.core.Loop": {
+    "constants": [], 
+    "extends": "java.lang.Object", 
+    "functions": [], 
+    "implements": [], 
+    "members": [
+      "exits"
+      ], 
+    "methods": [
+      "hookShutdown", 
+      "setLogger", 
+      "setPrecision", 
+      "now", 
+      "log", 
+      "concurrency", 
+      "precision", 
+      "scheduled", 
+      "schedule", 
+      "timeout", 
+      "collect", 
+      "dispatch"
+      ]
+    }, 
+  "org.async.core.Scheduled": {
+    "constants": [], 
+    "extends": "java.lang.Object", 
+    "functions": [], 
+    "implements": [
+      "java.lang.Comparable"
+      ], 
+    "members": [
+      "when"
+      ], 
+    "methods": [
+      "compareTo", 
+      "apply"
+      ]
+    }, 
+  "org.async.core.Static": {
+    "constants": [
+      "loop"
+      ], 
+    "extends": "java.lang.Object", 
+    "functions": [], 
+    "implements": [], 
+    "members": [], 
+    "methods": []
+    }, 
+  "org.async.core.Stream": {
+    "constants": [], 
+    "extends": "org.async.core.Dispatcher", 
+    "functions": [], 
+    "implements": [], 
+    "members": [], 
+    "methods": [
+      "push", 
+      "pull", 
+      "readable", 
+      "handleAccept", 
+      "handleWrite", 
+      "handleRead", 
+      "closeWhenDone", 
+      "collect", 
+      "produce"
+      ]
+    }, 
+  "org.async.net.Collector": {
+    "constants": [], 
+    "functions": [], 
+    "implements": [], 
+    "members": [], 
+    "methods": [
+      "collect", 
+      "terminate"
+      ]
+    }, 
+  "org.async.net.Dispatcher": {
+    "constants": [], 
+    "extends": "org.async.core.Stream", 
+    "functions": [], 
+    "implements": [], 
+    "members": [], 
+    "methods": [
+      "push", 
+      "handleCollect", 
+      "handleCollected", 
+      "collect", 
+      "writable", 
+      "produce"
+      ]
+    }, 
+  "org.async.simple.Bytes": {
+    "constants": [
+      "UTF8"
+      ], 
+    "extends": "java.lang.Object", 
+    "functions": [
+      "find", 
+      "encode", 
+      "decode"
+      ], 
+    "implements": [], 
+    "members": [], 
+    "methods": []
+    }, 
+  "org.async.simple.FileClassLoader": {
+    "constants": [], 
+    "extends": "java.lang.ClassLoader", 
+    "functions": [], 
+    "implements": [], 
+    "members": [], 
+    "methods": [
+      "findClass"
+      ]
+    }, 
+  "org.async.simple.Objects": {
+    "constants": [], 
+    "extends": "java.lang.Object", 
+    "functions": [
+      "extend", 
+      "list", 
+      "set", 
+      "iter", 
+      "update", 
+      "dict"
+      ], 
+    "implements": [], 
+    "members": [], 
+    "methods": []
+    }, 
+  "org.async.simple.Strings": {
+    "constants": [
+      "ALPHANUMERIC"
+      ], 
+    "extends": "java.lang.Object", 
+    "functions": [
+      "split", 
+      "join", 
+      "random"
+      ], 
+    "implements": [], 
+    "members": [], 
+    "methods": []
+    }
+  }
