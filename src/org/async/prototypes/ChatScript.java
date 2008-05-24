3ca5eb387c17678ec8396898320f4cab95ed32d6
laurentszyster <laurentszyster@ce82f516-9541-0410-b588-ed97c34d1957>
2008-05-24 14:02:22 +0000

    Reshuffled the method order to follow a client connection logic, added a constructor that sets buffers for a typical client: 16KB in, 4KB out.

--- src/org/async/prototypes/ChatScript.java
+++ src/org/async/prototypes/ChatScript.java
@@ -11,18 +11,28 @@ public final class ChatScript extends ChatDispatcher {
     private Function _handleTerminator = null;
     private Function _handleConnect = null;
     private Function _handleClose = null;
+    public ChatScript () {
+        super(16384, 4096); // buffer 16KB in, 4KB out
+    }
     protected final void _bind (ScriptableObject scope) {
         _scope = scope;
+        _handleConnect = Stateful._function(_scope, "handleConnect");
         _handleData = Stateful._function(_scope, "handleData");
         _handleTerminator = Stateful._function(_scope, "handleTerminator");
-        _handleConnect = Stateful._function(_scope, "handleConnect");
         _handleClose = Stateful._function(_scope, "handleClose");
+    }
+    public final void handleConnect() throws Throwable {
+        if (_handleConnect == null) {
+            throw new Exception("handleConnect not implemented");
+        } else {
+            Stateful._call(_scope, _handleConnect, new Object[]{this});
+        }
     }
     public final void handleData(byte[] data) throws Throwable {
         if (_handleData == null) {
             throw new Exception("handleData not implemented");
         } else {
-            Stateful._call(_scope, _handleData, new Object[]{data});
+            Stateful._call(_scope, _handleData, new Object[]{this, data});
         }
     }
     public final boolean handleTerminator() throws Throwable {
@@ -32,13 +42,6 @@ public final class ChatScript extends ChatDispatcher {
             return ((Boolean) Context.jsToJava(Stateful._call(
                 _scope, _handleTerminator, new Object[]{this}
                 ), Boolean.class)).booleanValue();
-        }
-    }
-    public final void handleConnect() throws Throwable {
-        if (_handleConnect == null) {
-            throw new Exception("handleConnect not implemented");
-        } else {
-            Stateful._call(_scope, _handleConnect, new Object[]{this});
         }
     }
     public final void handleClose() throws Throwable {
