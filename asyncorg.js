6244ddfb1dc38cfb5646a7f85a598d4179c8a26e
laurentszyster <laurentszyster@ce82f516-9541-0410-b588-ed97c34d1957>
2008-05-24 10:58:37 +0000

    Updated the asyncorg.js script to suite revision 73

--- asyncorg.js
+++ asyncorg.js
@@ -13,7 +13,7 @@ importClass(asyncorg.web.HttpServer);
 importClass(asyncorg.web.Authority);
 importClass(asyncorg.web.HttpServerState);
 importClass(asyncorg.web.FileSystem);
-importClass(asyncorg.web.Prototype);
+importClass(asyncorg.prototypes.WebScript);
 
 importClass(java.lang.Runtime);
 
@@ -21,7 +21,7 @@ var server = new HttpServer(".");
 
 var authority = new Authority("127.0.0.2", "/");
 
-var db = new AnSQLite(":memory:", 0);
+var db = new AnSQLite(":memory:");
 
 function _200_JSON_UTF8 (http, body) {
     http.responseHeader("Cache-control", "no-cache");
@@ -32,7 +32,7 @@ function _200_JSON_UTF8 (http, body) {
 function Service (model, service) {
     var _resource = JSON.pprint(model);
     var _type = JSONR.compile(_resource);
-    return Prototype.bind ({
+    return WebScript.bind ({
         "type": function (http) {return _type;},
         "service": service,
         "resource": function (http) {
@@ -42,7 +42,7 @@ function Service (model, service) {
 }
 
 function Resource (body) {
-    return Prototype.bind ({
+    return WebScript.bind ({
         "resource": function (http) {
             _200_JSON_UTF8 (http, body);
             }
@@ -53,40 +53,30 @@ var login = function (http) {
     http.identity = http.state.get("user");
     var password = http.state.get("pass");
     authority.identify(http, http.when()/1000);
-    _200_JSON_UTF8 (http, JSON.encode({
+    _200_JSON_UTF8(http, JSON.encode({
         "identity": http.identity, 
         "rights": http.rights
         }));
 }
 
+var logoff = function (http) {
+    _200_JSON_UTF8(http, JSON.encode(authority.unidentify(http)));
+}
+
 var ansql = function (http) {
-    _200_JSON_UTF8 (http, ansqlite.handle(http.state.getArray("arg0")));
+    _200_JSON_UTF8(http, ansqlite.handle(http.state.getArray("arg0")));
 }
 
 var inspect = function (http) {
-    _200_JSON_UTF8 (http, JSON.pprint(eval(
+    _200_JSON_UTF8(http, JSON.pprint(eval(
         '(' + http.state.getString("arg0") + ')'
         )));
 }
 
 var execute = function (http) {
-    _200_JSON_UTF8 (http, JSON.pprint(eval(
+    _200_JSON_UTF8(http, JSON.pprint(eval(
         "(function(){" + http.state.get("arg0", "null") + "})()"
         )));
-}
-
-var metabase_type = {
-    "Prototype": {
-        "type": function (http) {
-            return 
-        },
-        "service": function (http) {
-            
-        },
-        "resource": function (http) {
-            
-        }
-    }
 }
 
 var state = {
@@ -107,7 +97,7 @@ var state = {
                 "whenOut": channel.whenOut
                 });
         }
-        _200_JSON_UTF8 (http, JSON.pprint({
+        _200_JSON_UTF8(http, JSON.pprint({
             "accepted": server.channelsAccepted(),
             "accepting": server.readable(),
             "active": server.isActive(),
@@ -132,6 +122,7 @@ function Open(filename, address) {
         );
     var host = server.httpHost();
     server.httpRoute("GET " + host + "/", new FileSystem("www"));
+    server.httpRoute("GET " + host + "/doc", new FileSystem("doc"));
     server.httpRoute(
         "GET " + host + "/login", 
         Service({
@@ -140,12 +131,16 @@ function Open(filename, address) {
             }, login)
         );
     server.httpRoute(
+        "GET " + host + "/logoff", 
+        authority.identified(Service(null, logoff))
+        );
+    server.httpRoute(
         "POST " + host + "/db", 
         authority.identified(Service([null, null], ansql))
         );
     server.httpRoute(
         "GET " + host + "/state", 
-        authority.identified(Resource(state))
+        authority.identified(WebScript.bind(state))
         );
     server.httpRoute(
         "GET " + host + "/inspect", 
