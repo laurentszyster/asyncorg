9b92c8d964ce0a5494de3bb9ad0904eb22868e48
laurentszyster <laurentszyster@ce82f516-9541-0410-b588-ed97c34d1957>
2008-06-13 17:51:26 +0000

    Added a test web service that throws, catches and logs an exception to get an example of a stack trace.

--- test/HttpServerTest.java
+++ test/HttpServerTest.java
@@ -1,10 +1,35 @@
 import org.async.core.Static;
+import org.async.protocols.JSONR;
 import org.async.web.HttpServer;
 import org.async.web.FileSystem;
 import org.async.web.Database;
 import org.async.sql.AnSQLite;
+import org.async.web.Service;
+
 
 public class HttpServerTest {
+	protected static final class Except extends Service {
+		public final void configure (String route) {
+			;
+		}
+		public final boolean identify (HttpServer.Actor http) {
+			return false;
+		}
+		public final JSONR.Type type (HttpServer.Actor http) {
+			return null;
+		}
+		public final void service (HttpServer.Actor http) {
+			try {
+				throw new Exception("except");
+			} catch (Exception e) {
+				http.channel().log(e);
+			}
+			http.response(500); // Server Error
+		}
+		public final void resource (HttpServer.Actor http) {
+			http.response(501); // Not implemented
+		}
+	}
     public static void main (String[] args) throws Throwable {
         AnSQLite sql = new AnSQLite(":memory:", 0);
         Database db = new Database(sql);
@@ -17,6 +42,7 @@ public class HttpServerTest {
             server.httpRoute("GET " + host + "/db", db);
             server.httpRoute("POST " + host + "/db", db);
             server.httpRoute("GET " + host + "/doc", new FileSystem("doc"));
+            server.httpRoute("GET " + host + "/except", new Except());
             Static.loop.exits.add(server);
             server = null;
         } catch (Throwable e) {
