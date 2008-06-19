411e1131b84ada61c965df85f0363b6cfb989109
laurentszyster <laurentszyster@ce82f516-9541-0410-b588-ed97c34d1957>
2008-06-19 16:13:03 +0000

    A working DNS client to resolve A records.

--- src/org/async/dns/DNSClient.java
+++ src/org/async/dns/DNSClient.java
@@ -19,71 +19,22 @@
 
 package org.async.dns;
 
-import org.async.core.Dispatcher;
 import org.async.simple.Fun;
-import org.async.simple.Strings;
+import org.async.core.Timeouts;
+import org.async.core.Dispatcher;
 
+import java.util.Random;
 import java.util.Iterator;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.nio.ByteBuffer;
 
 public class DNSClient extends Dispatcher {
 
-    public static abstract class Request {
-        public int failover = 0;
-        public int ttl = 60;
-        public long when = 0;
-        public int uid = 0;
-        public SocketAddress peer;
-        public SocketAddress[] servers;
-        public byte[] response;
-        public ArrayList resources;
-        public ArrayList defered;
-        protected String[] _question;
-        protected Request(String[] question) {
-            _question = question;
-        }
-        protected final boolean unpack (byte[] datagram) {
-            response = datagram;
-            int ancount = (datagram[6]<<8) + datagram[7];
-            if (ancount > 0) {
-                int pos = DNS._skipName(datagram, 12);
-                resources = new ArrayList();
-                for (int an=0; an<ancount; an++) {
-                    pos = DNS._skipName(datagram, pos);
-                    if (collect(datagram, pos)) {
-                        break;
-                    }
-                    pos = pos + 8;
-                    pos = pos + 2 + (datagram[pos]<<8) + datagram[pos+1];
-                }
-                return collected(datagram, pos);
-            }
-            return false;
-        }
-        public abstract String[] template();
-        public final byte[] datagram () {
-            String[] t = template();
-            StringBuilder sb = new StringBuilder();
-            sb.append((char)(uid>>8)&0xff);
-            sb.append((char)uid&0xff);
-            sb.append(t[0]);
-            String part;
-            Iterator<String> parts = Strings.split(_question[0], '.');
-            while (parts.hasNext()) {
-                part = parts.next();
-                sb.append((char)part.length());
-                sb.append(part);
-            }
-            sb.append(t[1]);
-            return sb.toString().getBytes();
-        }
-        public abstract boolean collect(byte[] datagram, int pos);
-        public abstract boolean collected(byte[] datagram, int pos);
-    }
-    
-    public static final class RequestA extends Request {
+    public static final class RequestA extends DNSRequest {
         protected RequestA(String[] question) {
             super(question);
         }
@@ -96,17 +47,17 @@ public class DNSClient extends Dispatcher {
         }
         public final boolean collect(byte[] datagram, int pos) {
             if (
-                datagram[pos] == 0 && 
-                datagram[pos+1] == 1 && 
-                datagram[pos+2] == 0 && 
-                datagram[pos+3] == 1 
+                datagram[pos] == '\000' && 
+                datagram[pos+1] == '\001' && 
+                datagram[pos+2] == '\000' && 
+                datagram[pos+3] == '\001' 
                 ) {
                 ttl = DNS._unpackTTL(datagram, pos+4);
                 resources.add(
-                    Integer.toString(datagram[10]) + "." +
-                    Integer.toString(datagram[11]) + "." +
-                    Integer.toString(datagram[12]) + "." +
-                    Integer.toString(datagram[13])
+                    ((int) datagram[pos+10] & 0xff) + "." +
+                    ((int) datagram[pos+11] & 0xff) + "." +
+                    ((int) datagram[pos+12] & 0xff) + "." +
+                    ((int) datagram[pos+13] & 0xff)
                     );
                 return true;
             }
@@ -117,43 +68,74 @@ public class DNSClient extends Dispatcher {
         }
     }
     
+	protected final class DNSTimeouts extends Timeouts {
+		public DNSTimeouts (int period, int precision) {
+			super(period, precision);
+		}
+		@Override
+		public final void stop() {
+			close();
+		}
+		@Override
+		public final void timeout(Object reference) throws Throwable {
+			DNSRequest request = _pending.get(reference);
+			if (request == null) {
+				return;
+			}
+			request.failover++;
+			int L = request.servers.length;
+			if (request.failover <  L * _failover) {
+				request.peer = request.servers[request.failover % L];
+				dnsSend(request, _loop.now());
+			} else {
+				dnsContinue(request);
+			}
+		}
+
+	}
+	
+    protected DNSTimeouts _timeouts;
     protected long _sent = 0;
     protected int _failover = 1;
-    protected HashMap _cache = new HashMap();
-    protected HashMap _pending = new HashMap();
+    protected HashMap<String[], DNSRequest> _cache = new HashMap();
+    protected HashMap<Integer, DNSRequest> _pending = new HashMap();
     protected SocketAddress[] _servers;
     
-    public DNSClient(SocketAddress[] servers) {
+    public DNSClient(SocketAddress[] servers, int timeout, int precision) {
         super();
         _servers = servers;
+        _timeouts = new DNSTimeouts(timeout, precision);
     }
     
-    public void resolve(
-        String[] question, Fun resolved, SocketAddress[] servers
-        ) throws Throwable {
-        Request req = (Request) _cache.get(question);
-        if (req == null) {
-            if (req.defered == null) {
-                if (req.when + req.ttl > _loop.now()) {
-                    resolved.apply(req);
+    public final SocketAddress[] servers() {
+    	return _servers;
+    }
+    
+    public void resolve(String[] question, Fun resolved) throws Throwable {
+        DNSRequest request = _cache.get(question);
+        if (request != null) {
+            if (request.defered == null) {
+                if (request.when + request.ttl > _loop.now()) {
+                    resolved.apply(request);
                     return;
                 }
             } else {
-                req.defered.add(resolved);
+            	request.defered.add(resolved);
                 return;
             }
         }
         if (question == null || question.length != 2) {
             throw new Error("parameter must be an array of two strings");
         } else if (question[1] == "A") {
-            req = new RequestA(question);
+        	request = new RequestA(question);
         } else {
             throw new Error("unsupported DNS request type " + question[0]);
         }
-        req.servers = _servers;
-        req.defered = new ArrayList();
-        req.defered.add(resolved);
-        // send(req);
+        request.servers = _servers;
+        request.peer = _servers[0];
+        request.defered = new ArrayList();
+        request.defered.add(resolved);
+        dnsSend(request, _loop.now());
     }
     
     public boolean writable() {
@@ -176,13 +158,73 @@ public class DNSClient extends Dispatcher {
     }
 
     public void handleRead() throws Throwable {
+    	byte[] datagram = new byte[512];
+    	SocketAddress peer = recvfrom(ByteBuffer.wrap(datagram));
+    	if (peer == null) {
+    		return;
+    	} 
+    	Integer uid = ((datagram[0] & 0xff) * 256 + (datagram[1] & 0xff));
+    	if (!_pending.containsKey(uid)) {
+    		log("redundant " + peer.toString());
+    		return;
+    	}
+		DNSRequest request = _pending.remove(uid);
+		if (!request.peer.equals(peer)) {
+    		log("impersonate " + peer.toString());
+			return;
+		}
+		request.unpack(datagram);
+		dnsContinue(request);
     }
 
     public void handleClose() throws Throwable {
     }
 
     public Object apply(Object input) throws Throwable {
-        return null;
+    	_timeouts = null;
+        return Boolean.TRUE;
     }
 
+    public boolean dnsConnect() {
+    	try {
+        	String ip = InetAddress.getLocalHost().getHostAddress();
+        	int port = 1024 + (new Random()).nextInt(64507);
+    		bind(new InetSocketAddress(ip, port));
+    	} catch (Throwable e) {
+    		log(e);
+        	return false;
+    	}
+    	return true;
+    }
+    
+    public void dnsSend (DNSRequest request, long when) throws Throwable {
+    	if (_channel == null && !dnsConnect()) {
+			dnsContinue(request);
+			return;
+    	}
+    	request.when = when;
+    	Integer uid = request.uid = (int) (_sent % 65536);
+    	_pending.put(uid, request);
+    	sendto(ByteBuffer.wrap(request.datagram()), request.peer);
+    	_sent++;
+    	_timeouts.push(when, uid);
+    	log("sent " + uid);
+    }
+    
+    public void dnsContinue (DNSRequest request) {
+    	if (request.response != null) {
+    		request.response = null;
+    		_cache.put(request._question, request);
+    	}
+    	Iterator<Fun> defered = request.defered.iterator();
+    	request.defered = null;
+    	while (defered.hasNext()) {
+    		try {
+    			defered.next().apply(request);
+    		} catch (Throwable e) {
+    			log(e);
+    		}
+    	}
+    }
+    
 }
