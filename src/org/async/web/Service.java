5bd94426ae26867d2722eb302f6aac9869fa5adf
laurentszyster <laurentszyster@ce82f516-9541-0410-b588-ed97c34d1957>
2008-08-07 01:16:32 +0000

    Moved to a more functional web Service implementation
    By building service from a simple Function and a JSONR type it is possible to route and validate different types of request to the same generic function.

--- src/org/async/web/Service.java
+++ src/org/async/web/Service.java
@@ -3,27 +3,36 @@ package org.async.web;
 import org.async.web.HttpServer.Actor;
 import org.async.web.HttpServer.Handler;
 import org.async.collect.StringCollector;
-import org.async.protocols.JSON;
 import org.async.protocols.JSONR;
+import org.async.protocols.JSON;
+import org.async.simple.Fun;
 
 /**
  * The redefinition of a web service, using JSON in place of SOAP and
  * JSONR instead of WSDL.
  *
  */
-public abstract class Service implements Handler {
+public class Service implements Handler {
+	private Fun _function;
+    private JSONR.Type _type = null;
+    public Service (Fun function) {
+    	_function = function;
+    }
+    public Service (Fun function, JSONR.Type type) {
+    	_function = function;
+    	_type = type;
+    }
     public final boolean request(Actor http) throws Throwable {
         String method = http.method();
         if (method.equals("GET")) {
             String query = http.uri().getRawQuery();
             if (query != null) {
                 JSON.parseURLencoded(query, http.state);
-                JSONR.Type type = type(http);
-                if (type != null) {
-                    JSONR.validate(http.state, type);
+                if (_type != null) {
+                    JSONR.validate(http.state, _type);
                 }
             }
-            service(http);
+            _function.apply(http);
         } else if (method.equals("POST")) {
             http.collect(new StringCollector(new StringBuilder(), "UTF-8"));
         } else {
@@ -35,11 +44,10 @@ public abstract class Service implements Handler {
     public final void collected(Actor http) throws Throwable {
         Object input = null;
         String body = ((StringCollector) http.requestBody()).toString();
-        JSONR.Type type = type(http);
-        if (type == null) {
+        if (_type == null) {
             input = (new JSON()).eval(body);
         } else {
-            input = (new JSONR(type)).eval(body);
+            input = (new JSONR(_type)).eval(body);
         }
         if (input != null && input instanceof JSON.Object) {
             ((JSON.Object) input).putAll(http.state);
@@ -47,8 +55,6 @@ public abstract class Service implements Handler {
         } else {
             http.state.put("arg0", input);
         }
-        service(http);
+        _function.apply(http);
     }
-    public abstract JSONR.Type type (Actor http) throws Throwable;
-    public abstract void service (Actor http) throws Throwable;
 }
